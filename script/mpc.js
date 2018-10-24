fdaf0d6f4907db5468f0a14b211370843e1d6f01
Andrew Lovett-Barron <andrew@relaystudio.com>
2018-10-24 17:23:34 +0200

    Fix

--- script/mpc.js
+++ script/mpc.js
@@ -112,7 +112,7 @@ soundManager.setup({
     }
 
     // Load in Ingr
-    var soundURLs = 'i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16, i17, i18, i19, i20, i21, i22,  i23, i24, i25, i26, i27, i28, i29, i30, i31'.split(',');
+    var soundURLs = 'i0,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15,i16,i17,i18,i19,i20,i21,i22,i23,i24,i25,i26,i27,i28,i29,i30,i31'.split(',');
     for (var i = 0; i < soundURLs.length; i++) {
       soundManager.createSound({
         id: 'i' + i,
